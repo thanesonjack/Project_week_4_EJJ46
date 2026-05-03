8c7bef934a2079b0626d62cef9f02ec34940303d
EJJ46 <ejj46@bath.ac.uk>
2026-05-03 13:57:35 +0000

    Improved Lvl2 code, and implemented difficuty selection for Lvl3

--- Game/Levels/Lvl2.py
+++ Game/Levels/Lvl2.py
@@ -75,41 +75,57 @@ def monsterinput():
 
     moves = possiblemoves()
 
+    if not moves:
+        return
+
     r,c = random.choice(moves)
     tictactoe[r][c] = "O"
 
 
 def fullboard():
-    if len(possiblemoves()) == 0:
-        print("It's a draw!")
-        return True
-    return False
+    if not wincondition():
+        if len(possiblemoves()) == 0:
+            print("It's a draw!")
+            return True
+        return False
     
 def wincondition():
+    Lvl3difficulty = 0
+
     if tictactoe[0][0] == "X" and tictactoe[0][1] == "X" and tictactoe[0][2] == "X":
-        print("You win!")
-        return True
+        return "X"
     elif tictactoe[1][0] == "X" and tictactoe[1][1] == "X" and tictactoe[1][2] == "X":
-        print("You win!")
-        return True
+        return "X"
+    elif tictactoe[1][0] == "X" and tictactoe[1][1] == "X" and tictactoe[1][2] == "X":
+        return "X"
     elif tictactoe[2][0] == "X" and tictactoe[2][1] == "X" and tictactoe[2][2] == "X":
-        print("You win!")
-        return True
+        return "X"
     elif tictactoe[0][0] == "X" and tictactoe[1][0] == "X" and tictactoe[2][0] == "X":
-        print("You win!")
-        return True
+        return "X"
     elif tictactoe[0][1] == "X" and tictactoe[1][1] == "X" and tictactoe[2][1] == "X":
-        print("You win!")
-        return True
+        return "X"
     elif tictactoe[0][2] == "X" and tictactoe[1][2] == "X" and tictactoe[2][2] == "X":
-        print("You win!")
-        return True
+        return "X"
     elif tictactoe[0][0] == "X" and tictactoe[1][1] == "X" and tictactoe[2][2] == "X":
-        print("You win!")
-        return True
+        return "X"
     elif tictactoe[0][2] == "X" and tictactoe[1][1] == "X" and tictactoe[2][0] == "X":
-        print("You win!")
-        return True
+        return "X"
+    elif tictactoe[0][0] == "O" and tictactoe[0][1] == "O" and tictactoe[0][2] == "O":
+        return "O"
+    elif tictactoe[1][0] == "O" and tictactoe[1][1] == "O" and tictactoe[1][2] == "O":
+        return "O"
+    elif tictactoe[2][0] == "O" and tictactoe[2][1] == "O" and tictactoe[2][2] == "O":
+        return "O"
+    elif tictactoe[0][0] == "O" and tictactoe[1][0] == "O" and tictactoe[2][0] == "O":
+        return "O"
+    elif tictactoe[0][1] == "O" and tictactoe[1][1] == "O" and tictactoe[2][1] == "O":
+        return "O"
+    elif tictactoe[0][2] == "O" and tictactoe[1][2] == "O" and tictactoe[2][2] == "O":
+        return "O"
+    elif tictactoe[0][0] == "O" and tictactoe[1][1] == "O" and tictactoe[2][2] == "O":
+        return "O"
+    elif tictactoe[0][2] == "O" and tictactoe[1][1] == "O" and tictactoe[2][0] == "O":
+        return "O"
     else:
         return False
 
@@ -119,17 +135,38 @@ def run():
 
     print("You made it through level 1? Well, you're not done yet! In this level, you need to play the monster at tic tac toe to make it through. You go first, good luck!")
 
-    while not wincondition():
+    while True:
         printboard()
         playerinput()
+        
+        winner = wincondition()
+        if winner == "X":
+            printboard()
+            print("Congratulations, you win! The next level may be slightly easier...")
+            return 1       #easy difficulty
+
         if fullboard():
-            break
-            return False
+            Lvl3difficulty = 2
+            printboard()
+            print("It's a draw. Progress if you dare...")
+            return 2     #medium difficulty
+
         monsterinput()
-    if wincondition():
-        printboard()
-        print("You win! Level complete!")
-        return True
+
+        winner = wincondition()
+        if winner == "O":
+            Lvl3difficulty = 3
+            printboard()
+            print("The monster wins! You may struggle with the next level...")
+            return 3     #hard difficulty
+
+        if fullboard():
+            Lvl3difficulty = 2
+            printboard()
+            print("It's a draw. Progress if you dare...")
+            return 2     #medium difficulty
+            
+
     
 
 run()
